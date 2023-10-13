90efbdaa96affea399b8bcce228da3640d074710
NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>
2023-10-13 23:44:35 +0200

    kiirja a parancssori paraméterként kapott szám fibonacci értékét

--- alap.cpp
+++ alap.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
+constexpr int fib(int n)
 {
-	
+	return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char* argv[])
+{	
+	if (argc < 2)
+	{
+		cout << "Usage: " << argv[0] << " <number>" << endl;
+		return 1;
+	}
+
+	int n = atoi(argv[1]);
+
+	cout << "fib(" << n << ") = " << fib(n) << endl;
 	return 0;
 }
