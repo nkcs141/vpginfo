b1404e5c5f0420a7e2b870d2cdf7d9374c469373
nkcs141 <147881807+nkcs141@users.noreply.github.com>
2023-12-01 14:27:34 +0100

    Add files via upload

---
+++ fagyos_idoszakok_szama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++)
+    int meres;
+    cin >> meres;
+    string fagyos_idoszak;
+    {
+        if(meres<0)
+    }
+
+    cout << fagyos_idoszak;
+
+	return 0;
+}
